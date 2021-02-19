2d8c6b7c8e242ccd02e25e6bd70732a34b7d1da4
Federico Peliti <federico.peliti@gmail.com>
2021-02-19 13:11:54 +0100

    fix foo

--- js/foo.js
+++ js/foo.js
@@ -1,24 +1,27 @@
-AFRAME.registerComponent('foo', {
-    init: function() {
-this.vid = document.querySelector("#video1");
+AFRAME.registerComponent("foo", {
+    init: function () {
+        this.vid = document.querySelector("#video1");
+    },
+    tick: function () {
+        if (this.el.sceneEl.camera) {
+            var cam = this.el.sceneEl.camera;
+            var frustum = new THREE.Frustum();
+            frustum.setFromMatrix(
+                new THREE.Matrix4().multiplyMatrices(
+                    cam.projectionMatrix,
+                    cam.matrixWorldInverse
+                )
+            );
 
-    }
-    tick: function() {
-     if (this.el.sceneEl.camera) {
-        var cam = this.el.sceneEl.camera
-        var frustum = new THREE.Frustum();
-        frustum.setFromMatrix(new THREE.Matrix4().multiplyMatrices(cam.projectionMatrix, 
-        cam.matrixWorldInverse));  
-  
-        // Your 3d point to check
-        var pos = new THREE.Vector3();
-        this.el.object3D.getWorldPosition(pos)
-        if (frustum.containsPoint(pos)) {
-          // Do something with the position...
-          this.vid.pause();
-        } else {
-            this.vid.play();
+            // Your 3d point to check
+            var pos = new THREE.Vector3();
+            this.el.object3D.getWorldPosition(pos);
+            if (frustum.containsPoint(pos)) {
+                // Do something with the position...
+                this.vid.pause();
+            } else {
+                this.vid.play();
+            }
         }
-     }
-    }
-  }
+    },
+});
