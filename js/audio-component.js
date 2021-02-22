dab19b5058dec093770c1e9bb0e3ca248125bdf8
Federico Peliti <federico.peliti@gmail.com>
2021-02-22 15:28:02 +0100

    herman

---
+++ js/audio-component.js
@@ -0,0 +1,35 @@
+AFRAME.registerComponent("foo", {
+    init: function () {
+        this.vid = document.querySelector("#video1");
+    },
+    tick: function () {
+        if (this.el.sceneEl.camera) {
+            var cam = this.el.sceneEl.camera;
+            var frustum = new THREE.Frustum();
+            frustum.setFromMatrix(
+                new THREE.Matrix4().multiplyMatrices(
+                    cam.projectionMatrix,
+                    cam.matrixWorldInverse
+                )
+            );
+
+            // Your 3d point to check
+            var pos = new THREE.Vector3();
+            this.el.object3D.getWorldPosition(pos);
+
+            if (frustum.containsPoint(pos)) {
+                // Do something with the position...
+                window.onclick = function () {
+                    var video1 = document.querySelector("#video1");
+                    var video1 = document.querySelector("#video2");
+
+                    video1.muted = !video1.muted;
+                    console.log(this.el.getAttribute("material").src);
+                    console.log(this.el.getDOMAttribute("material").src);
+                    var btnTxt = document.getElementById("displayBtn");
+                    btnTxt.innerHTML = "Sound Off";
+                };
+            }
+        }
+    },
+});
