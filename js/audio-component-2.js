c4ae45df478c085e582c1eb3b19344d199ce5d58
Federico Peliti <federico.peliti@gmail.com>
2021-02-22 21:49:04 +0100

    mezzo revert

--- js/audio-component-2.js
+++ js/audio-component-2.js
@@ -18,22 +18,12 @@ AFRAME.registerComponent("audiohandler2", {
             this.el.object3D.getWorldPosition(pos);
 
             if (frustum.containsPoint(pos)) {
-                //document.getElementById(btn2).style.display = "block";
-                // Do something with the position...
-
                 window.onclick = function () {
                     var video1 = document.querySelector("#video1");
                     var video2 = document.querySelector("#video2");
-                    //var videoSrc = this.el.object3D.getAttribute("material");
-                    //var videoSrc = document.querySelector("#roma");
-                    //var videoMat = videoSrc.getAttribute("material").src;
 
                     video2.muted = !video2.muted;
                     video1.muted = true;
-
-                    //console.log(videoSrc.getAttribute("material").src);
-                    //var btnTxt = document.getElementById("displayBtn");
-                    //btnTxt.innerHTML = "Sound Off";
                 };
             }
         }
