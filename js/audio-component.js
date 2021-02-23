58c25844f439e1cb0c64ac455aa0c5580c806bc2
Federico Peliti <federico.peliti@gmail.com>
2021-02-23 11:47:06 +0100

    this.el.getDOMAttribute

--- js/audio-component.js
+++ js/audio-component.js
@@ -24,7 +24,7 @@ AFRAME.registerComponent("audiohandler", {
                     var video1 = document.querySelector("#video1");
                     var video2 = document.querySelector("#video2");
                     //var mat = document.querySelector("#videoPl1");
-                    var mat = this.el.object3D.getDOMAttribute("material").src;
+                    var mat = this.el.getDOMAttribute("material").src;
                     video1.muted = !video1.muted;
                     video2.muted = true;
                     //console.log(mat.getAttribute("material").src);
